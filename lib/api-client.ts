9d4045aa9464dc5c72a53091b416534d852c8cb5
Nathan Hettige <nathan.hettige@gmail.com>
2025-09-02 10:50:10 +1000

    feat: initial setup of tanstack query

---
+++ lib/api-client.ts
@@ -0,0 +1,22 @@
+import { QueryCache, QueryClient } from "@tanstack/react-query";
+
+/**
+ * Create a query client for React Query
+ *
+ * @see https://tanstack.com/query/latest/docs/framework/react/guides/ssr#initial-setup
+ */
+export const createQueryClient = () =>
+  new QueryClient({
+    defaultOptions: {
+      queries: {
+        // With SSR, we usually want to set some default staleTime
+        // above 0 to avoid refetching immediately on the client
+        staleTime: 60 * 1000,
+      },
+    },
+    queryCache: new QueryCache({
+      onError: (err) => {
+        // global error handling, e.g. toast notification ...
+      },
+    }),
+  });
