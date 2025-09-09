1965b65c43a3fbc78ba9b4c2e2edaa86255397ba
Hugh Henry <hhenry@deloitte.com.au>
2025-09-09 14:41:46 +1000

    feat: Move components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Navbar } from "@/components/molecules/navbar";
+import { Navbar } from "@/components/features/layout/navbar";
 import { Providers } from "@/components/providers";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
