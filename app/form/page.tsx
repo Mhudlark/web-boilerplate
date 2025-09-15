b40e670a3c0703b3c9f7bf78d8d00fd692104f7c
Hugh Henry <hhenry@deloitte.com.au>
2025-09-15 16:45:36 +1000

    feat: Remove main element

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -2,11 +2,11 @@ import ExampleFormPage from "./example-form-page";
 
 export default function FormPage() {
   return (
-    <main className="bg-background flex min-h-screen justify-center p-8">
+    <div className="bg-background flex min-h-screen justify-center p-8">
       <div className="flex max-w-4xl flex-col gap-24">
         <h1 className="text-center text-4xl font-bold">Forms</h1>
         <ExampleFormPage />
       </div>
-    </main>
+    </div>
   );
 }
